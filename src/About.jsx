654479ff41a570e5be8cd488b2340bec2180389d
Pritom-10 <pritomshiro@gmail.com>
2026-03-10 20:55:13 +0600

    sdf

--- src/About.jsx
+++ src/About.jsx
@@ -4,52 +4,66 @@ import theme_patter from './assets/images/theme_pattern.svg'
 import profile_img1 from './assets/images/profile.png'
 const About=()=>
 {
-    return(
-        <div id="about" className="about">
-            <div className="about-title">
-                <h1>About me</h1>
-                <img src={theme_patter} alt="" srcset="" />
-                </div>
-                <div className="about-section">
-                    <div className="about-left">
-                        <img style={{width:"300px"}} src={profile_img1} alt="" srcset="" />
-                    </div>
-                    <div className="about-right">
-                        <div className="about-para">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam eos laudantium dolores eius hic perferendis expedita itaque </p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus a non assumenda.</p>
-                        </div>
-                        <div className="about-skills">
-                            <div className="about-skill">
-                                <p>HTML & CSS</p><hr style={{width:"60%"}} /> </div>
-                            <div className="about-skill">
-                                <p>React JS</p><hr style={{width:"70%"}} /> </div>
-                         <div className="about-skill">
-                                <p>JAVA</p><hr style={{width:"60%"}} /> </div>
-                            <div className="about-skill">
-                                <p>Javascript</p><hr style={{width:"70%"}} /> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="about-achievements">
-                    <div className="about-achievement">
-                        <h1>10+</h1>
-                        <p>YEARS OF EXPEIENCE</p>
-                    </div>
-                    <hr />
-                    <div className="about-achievement">
-                        <h1>90+</h1>
-                        <p>PROJECT COMPLETED</p>
-                    </div>
-                    <hr />
-                    <div className="about-achievement">
-                        <h1>15+</h1>
-                        <p>HAPPY CLIENTS</p>
-                    </div>
-               
-            </div>
+    return (
+      <div id="about" className="about">
+        <div className="about-title">
+          <h1>About me</h1>
+          <img src={theme_patter} alt="" srcset="" />
         </div>
-    )
+        <div className="about-section">
+          <div className="about-left">
+            <img
+              style={{ width: "300px" }}
+              src={profile_img1}
+              alt=""
+              srcset=""
+            />
+          </div>
+          <div className="about-right">
+            <div className="about-para">
+              <p>
+                I am a passionate Frontend Developer who enjoys building modern,
+                responsive, and user-friendly web applications. I like turning
+                ideas into real products by writing clean and structured code.
+              </p>
+            </div>
+            <div className="about-skills">
+              <div className="about-skill">
+                <p>HTML & CSS</p>
+                <hr style={{ width: "60%" }} />{" "}
+              </div>
+              <div className="about-skill">
+                <p>React JS</p>
+                <hr style={{ width: "70%" }} />{" "}
+              </div>
+              <div className="about-skill">
+                <p>JAVA</p>
+                <hr style={{ width: "60%" }} />{" "}
+              </div>
+              <div className="about-skill">
+                <p>Javascript</p>
+                <hr style={{ width: "70%" }} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="about-achievements">
+          <div className="about-achievement">
+            <h1>10+</h1>
+            <p>YEARS OF EXPEIENCE</p>
+          </div>
+          <hr />
+          <div className="about-achievement">
+            <h1>90+</h1>
+            <p>PROJECT COMPLETED</p>
+          </div>
+          <hr />
+          <div className="about-achievement">
+            <h1>15+</h1>
+            <p>HAPPY CLIENTS</p>
+          </div>
+        </div>
+      </div>
+    );
 }
 export default About
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -3,16 +3,34 @@ import './assets/css/Hero.css'
 import profile_img from './assets/images/profile.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 const Hero=()=>{
-    return(
-        <div id="home" className="hero">
-            <img style={{borderRadius:'2000px',width:"200px",marginTop:"150px"}} src={profile_img}alt="" />
-            <h1><span>I am Pritom Barua Shiro,</span>frontend developer based in BD.</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique deserunt totam libero quidem, ipsum nesciunt suscipit facere quibusdam deleniti aliquam at itaque, </p>
-            <div className="hero-section">
-                <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact' ></AnchorLink>Connect wite me</div>
-                <div className="hero-resume">My resume</div>
-            </div>
+    return (
+      <div id="home" className="hero">
+        <img
+          style={{ borderRadius: "2000px", width: "200px", marginTop: "150px" }}
+          src={profile_img}
+          alt=""
+        />
+        <h1>
+          <span>I am Pritom Barua Shiro,</span>frontend developer based in BD.
+        </h1>
+        <p>
+          I help businesses and individuals turn their ideas into fast, modern,
+          and interactive websites. My goal is not just to write code, but to
+          create solutions that improve user experience and make products easier
+          to use.
+        </p>
+        <div className="hero-section">
+          <div className="hero-connect">
+            <AnchorLink
+              className="anchor-link"
+              offset={50}
+              href="#contact"
+            ></AnchorLink>
+            Connect wite me
+          </div>
+          <div className="hero-resume">My resume</div>
         </div>
-    )   
+      </div>
+    );   
 }
 export default Hero
